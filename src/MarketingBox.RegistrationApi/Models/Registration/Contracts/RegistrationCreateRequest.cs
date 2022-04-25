02ebb825f75d0cf3f71400e9dbe78d8ec1ff30ac
Lev Kurbanov <lev.k@smplt.net>
2022-04-25 14:13:42 +0500

    update model

--- src/MarketingBox.RegistrationApi/Models/Registration/Contracts/RegistrationCreateRequest.cs
+++ src/MarketingBox.RegistrationApi/Models/Registration/Contracts/RegistrationCreateRequest.cs
@@ -1,4 +1,6 @@
-﻿using Destructurama.Attributed;
+﻿using System.ComponentModel.DataAnnotations;
+using Destructurama.Attributed;
+using MarketingBox.Sdk.Common.Attributes;
 using MarketingBox.Sdk.Common.Enums;
 
 namespace MarketingBox.RegistrationApi.Models.Registration.Contracts
@@ -6,38 +8,62 @@ namespace MarketingBox.RegistrationApi.Models.Registration.Contracts
     public class RegistrationCreateRequest
     {
         #region Personal Info
+
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
         [LogMasked(PreserveLength = true, ShowFirst = 2, ShowLast = 2)]
         public string FirstName { get; set; }
 
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
         [LogMasked(PreserveLength = true, ShowFirst = 2, ShowLast = 2)]
         public string LastName { get; set; }
-        
+
+        [Required]
+        [IsValidPassword]
+        [StringLength(128, MinimumLength = 6)]
         [LogMasked(PreserveLength = true)]
         public string Password { get; set; }
-        
+
+        [Required]
+        [IsValidEmail]
         [LogMasked(PreserveLength = true, ShowFirst = 2, ShowLast = 2)]
         public string Email { get; set; }
-        
+
+        [Required]
         [LogMasked(PreserveLength = true, ShowFirst = 2, ShowLast = 2)]
         public string Phone { get; set; }
-        
+
+        [Required]
         [LogMasked(PreserveLength = true, ShowFirst = 2, ShowLast = 2)]
         public string Ip { get; set; }
-        
-        public string CountryCode { get; set; }
+
+        [Required]
+        [IsEnum]
         public CountryCodeType? CountryCodeType { get; set; }
-        
+
+        [Required]
+        [StringLength(3, MinimumLength = 2)]
+        public string CountryCode { get; set; }
+
         #endregion
+
         #region Route Info
+
         // Old name "offerId"
+        [Required]
         public long CampaignId { get; set; }
+
         // Old name "So"
         public string Funnel { get; set; }
+
         // Old name "sub"
         public string AffCode { get; set; }
+
         #endregion
 
         #region Additional parameters
+
         public string Sub1 { get; set; }
         public string Sub2 { get; set; }
         public string Sub3 { get; set; }
@@ -49,6 +75,6 @@ namespace MarketingBox.RegistrationApi.Models.Registration.Contracts
         public string Sub9 { get; set; }
         public string Sub10 { get; set; }
 
-        #endregion  
+        #endregion
     }
 }
